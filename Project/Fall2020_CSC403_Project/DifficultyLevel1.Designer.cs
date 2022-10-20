0382e2b9f88b03372118acae28e9ad951e8b3a08
Jannatul Ferdousi <jferdousi944@gmail.com>
2022-10-20 01:17:55 -0500

    Made DifficultyLevel1.cs form, added several button, linked btton to form
    I linked the DifficultyLevel.cs from the SELECT LEVEL button from the StartMenue.cs form so the DifficultyLevel1 form can show up by clicking SELECT LEVEL button. I created several buttons in the DifficultyLevel1.cs form. I made an button named Easy, and on clicking the Easy button, the FrmLevel1.cs quickly showed up. In addition, I changed DifficultyLevel.cs form's background.

---
+++ Project/Fall2020_CSC403_Project/DifficultyLevel1.Designer.cs
@@ -0,0 +1,103 @@
+﻿namespace Fall2020_CSC403_Project
+{
+    partial class DifficultyLevel1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnEasy = new System.Windows.Forms.Button();
+            this.btnMedium = new System.Windows.Forms.Button();
+            this.btnCancel = new System.Windows.Forms.Button();
+            this.btnHard = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnEasy
+            // 
+            this.btnEasy.BackColor = System.Drawing.SystemColors.ScrollBar;
+            this.btnEasy.Location = new System.Drawing.Point(220, 77);
+            this.btnEasy.Name = "btnEasy";
+            this.btnEasy.Size = new System.Drawing.Size(364, 70);
+            this.btnEasy.TabIndex = 0;
+            this.btnEasy.Text = "Easy";
+            this.btnEasy.UseVisualStyleBackColor = false;
+            this.btnEasy.Click += new System.EventHandler(this.btnEasy_Click);
+            // 
+            // btnMedium
+            // 
+            this.btnMedium.AccessibleRole = System.Windows.Forms.AccessibleRole.Application;
+            this.btnMedium.BackColor = System.Drawing.SystemColors.HotTrack;
+            this.btnMedium.Location = new System.Drawing.Point(220, 153);
+            this.btnMedium.Name = "btnMedium";
+            this.btnMedium.Size = new System.Drawing.Size(364, 70);
+            this.btnMedium.TabIndex = 1;
+            this.btnMedium.Text = "Medium";
+            this.btnMedium.UseVisualStyleBackColor = false;
+            // 
+            // btnCancel
+            // 
+            this.btnCancel.BackColor = System.Drawing.SystemColors.Info;
+            this.btnCancel.Location = new System.Drawing.Point(218, 302);
+            this.btnCancel.Name = "btnCancel";
+            this.btnCancel.Size = new System.Drawing.Size(364, 70);
+            this.btnCancel.TabIndex = 2;
+            this.btnCancel.Text = "Cancel";
+            this.btnCancel.UseVisualStyleBackColor = false;
+            // 
+            // btnHard
+            // 
+            this.btnHard.AccessibleRole = System.Windows.Forms.AccessibleRole.Application;
+            this.btnHard.BackColor = System.Drawing.SystemColors.InactiveCaption;
+            this.btnHard.Location = new System.Drawing.Point(218, 226);
+            this.btnHard.Name = "btnHard";
+            this.btnHard.Size = new System.Drawing.Size(364, 70);
+            this.btnHard.TabIndex = 3;
+            this.btnHard.Text = "Hard";
+            this.btnHard.UseVisualStyleBackColor = false;
+            // 
+            // DifficultyLevel1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::Fall2020_CSC403_Project.Properties.Resources.wall;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnHard);
+            this.Controls.Add(this.btnCancel);
+            this.Controls.Add(this.btnMedium);
+            this.Controls.Add(this.btnEasy);
+            this.Name = "DifficultyLevel1";
+            this.Text = "DifficultyLevel1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnEasy;
+        private System.Windows.Forms.Button btnMedium;
+        private System.Windows.Forms.Button btnCancel;
+        private System.Windows.Forms.Button btnHard;
+    }
+}
