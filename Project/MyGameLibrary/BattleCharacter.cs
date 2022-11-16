d73f119b64067f354e75c526d3269cea1ef623f5
MantashaNoyela <mantashanoyela4@gmail.com>
2022-11-16 12:19:58 -0600

    health

--- Project/MyGameLibrary/BattleCharacter.cs
+++ Project/MyGameLibrary/BattleCharacter.cs
@@ -27,5 +27,10 @@ namespace Fall2020_CSC403_Project.code {
     public void AlterHealth(int amount) {
       Health += amount;
     }
-  }
+
+        public void Maxhp()
+        {
+            Health = 20;
+        }
+    }
 }
